85f9bcf63e19eaf15f491d44291f247497f68c68
Adam Martin <a_martin15@hotmail.com>
2022-05-12 20:43:17 +0100

    Backend challenge changes/additions

--- server/database/EventsRepository.js
+++ server/database/EventsRepository.js
@@ -1,9 +1,15 @@
-const eventData = require('./events.json');
+const eventData = require("./events.json")
+const threatLevels = require("../constants/ThreatLevels")
 
 const EventsRepository = () => ({
   async getAll(sortField) {
-    return new Promise((resolve) => resolve(eventData));
+    const dataAsc = eventData.sort(({[sortField]: a}, {[sortField]: b}) => a-b)
+    return new Promise((resolve) => resolve(dataAsc))
+    // return new Promise((resolve) => resolve(eventData))
   },
-});
+  async getThreatLevels() {
+    return new Promise((resolve) => resolve(threatLevels))
+  },
+})
 
-module.exports = EventsRepository();
+module.exports = EventsRepository()
--- server/services/events/EventsService.js
+++ server/services/events/EventsService.js
@@ -1,9 +1,22 @@
-const eventsRepo = require('../../database/EventsRepository');
+const eventsRepo = require("../../database/EventsRepository")
+const seedsRepo = require("../../database/SeedsRepository")
 
 const EventsService = () => ({
-  async getAll(sortField = 'seedId') {
-    return eventsRepo.getAll(sortField);
-  },
-});
+  async getAll(sortField = "seedId") {
+    const events = await eventsRepo.getAll(sortField)
+    const threatLevels = await eventsRepo.getThreatLevels()
+    const seeds = await seedsRepo.getAll()
 
-module.exports = EventsService();
+    const eventsWithThreatAndSeed = events.map((event) => {
+      return {
+        ...event,
+        threatLevel: threatLevels[event.threatLevelCode],
+        seed: seeds.find((seed) => seed.id === event.seedId),
+      }
+    })
+    // console.log("eventsWithThreatAndSeed: ", eventsWithThreatAndSeed)
+    return eventsWithThreatAndSeed
+  },
+})
+
+module.exports = EventsService()
